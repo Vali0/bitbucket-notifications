4d97dd9f5e0e8115d1824e70885a1f2feb3678f1
Valentin <valentin.radev@blue-leaf.co.uk>
2017-12-18 20:24:36 +0200

    Renamed package name. Added index file. Added configuration file.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-exports.Client = require('./lib/Client');
+function BitbucketNotifications() {
+    'use strict';
+
+    const configuration = require('./config');
+    const Client = require('./lib/Client');
+    const Gmail = require('./lib/Gmail');
+
+    if (!(configuration.bitbucket.clientId && configuration.bitbucket.clientSecret && configuration.bitbucket.accessToken && configuration.bitbucket.refreshToken)) {
+        throw new Error('Missing OAuth2 configuration for bitbucket in config.json');
+    }
+
+    if (!(configuration.gmail.user && configuration.gmail.clientId && configuration.gmail.clientSecret && configuration.gmail.accessToken && configuration.gmail.refreshToken)) {
+        throw new Error('Missing OAuth2 configuration for gmail configuration.gmail.clientSecret');
+    }
+
+    var client = new Client(configuration.bitbucket.clientId, configuration.bitbucket.clientSecret, configuration.bitbucket.accessToken, configuration.bitbucket.refreshToken);
+    var gmail = new Gmail(configuration.gmail.user, configuration.gmail.clientId, configuration.gmail.clientSecret, configuration.gmail.refreshToken);
+
+    return {
+    	bitbucketClient: client,
+    	gmail: gmail
+    };
+}
+
+exports.BitbucketNotifications = BitbucketNotifications;
