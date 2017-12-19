de04cbbe33c31b364335ead95642ea63f5912346
Valentin <valentin.radev@blue-leaf.co.uk>
2017-12-19 10:30:22 +0200

    uploading configuration

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 function BitbucketNotifications() {
     'use strict';
 
-    const configuration = require('/config');
+    const config = require('config');
+    const bitbucketConfig = config.get('bitbucket');
+    const gmailConfig = config.get('gmail');
     const Client = require('./lib/Client');
     const Gmail = require('./lib/Gmail');
 
-    if (!(configuration.bitbucket.clientId && configuration.bitbucket.clientSecret && configuration.bitbucket.accessToken && configuration.bitbucket.refreshToken)) {
+    if (!(bitbucketConfig.clientId && bitbucketConfig.clientSecret && bitbucketConfig.accessToken && bitbucketConfig.refreshToken)) {
         throw new Error('Missing OAuth2 configuration for bitbucket in config.json');
     }
 
-    if (!(configuration.gmail.user && configuration.gmail.clientId && configuration.gmail.clientSecret && configuration.gmail.accessToken && configuration.gmail.refreshToken)) {
+    if (!(gmailConfig.user && gmailConfig.clientId && gmailConfig.clientSecret && gmailConfig.accessToken && gmailConfig.refreshToken)) {
         throw new Error('Missing OAuth2 configuration for gmail configuration.gmail.clientSecret');
     }
 
-    var client = new Client(configuration.bitbucket.clientId, configuration.bitbucket.clientSecret, configuration.bitbucket.accessToken, configuration.bitbucket.refreshToken);
-    var gmail = new Gmail(configuration.gmail.user, configuration.gmail.clientId, configuration.gmail.clientSecret, configuration.gmail.refreshToken);
+    var client = new Client(bitbucketConfig.clientId, bitbucketConfig.clientSecret, bitbucketConfig.accessToken, bitbucketConfig.refreshToken);
+    var gmail = new Gmail(gmailConfig.user, gmailConfig.clientId, gmailConfig.clientSecret, gmailConfig.refreshToken);
 
     return {
         bitbucketClient: client,
         gmail: gmail
     };
 }
+
+// var test = BitbucketNotifications();
+// console.log(test);
 
 exports.BitbucketNotifications = BitbucketNotifications;
