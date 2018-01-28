a81e3f0d33904629e4411fa87573850cb30c684d
Vali0 <valentin.radev@blue-leaf.co.uk>
2018-01-28 19:38:00 +0200

    Added mocha and chai as dev dependencies. Added unit tests for Bitbucket.js

---
+++ test/Bitbucket.js
@@ -0,0 +1,74 @@
+var expect = require('chai').expect;
+var Bitbucket = require('../lib/Bitbucket');
+
+describe('Bitbucket', function() {
+  describe('constructor', function() {
+    let clientId,
+        clientSecret,
+        accessToken,
+        refreshToken;
+
+    beforeEach(function () {
+        clientId = 'clientId';
+        clientSecret = 'clientSecret';
+        accessToken = 'accessToken';
+        refreshToken = 'refreshToken';
+    });
+
+    it('it should throw an error if client id is missing', function() {
+        // arrange
+
+        // act
+        let client = function() {new Bitbucket()};
+
+        // assert
+        expect(client).to.throw('Client id is missing');
+    });
+
+    it('it should throw an error if client secret is missing', function() {
+        // arrange
+
+        // act
+        let client = function() {new Bitbucket(clientId)};
+
+        // assert
+        expect(client).to.throw('Client secret is missing');
+    });
+
+    it('it should throw an error if access token is missing', function() {
+        // arrange
+
+        // act
+        let client = function() {new Bitbucket(clientId, clientSecret)};
+
+        // assert
+        expect(client).to.throw('Access token is missing');
+    });
+
+    it('it should throw an error if refresh token is missing', function() {
+        // arrange
+
+        // act
+        let client = function() {new Bitbucket(clientId, clientSecret, accessToken)};
+
+        // assert
+        expect(client).to.throw('Refresh token is missing');
+    });
+
+    it('it should return bitbucket instance if all parameters are passed correctly', function() {
+        // arrange
+
+        // act
+        let client = new Bitbucket(clientId, clientSecret, accessToken, refreshToken);
+
+        // assert
+        expect(client).to.be.ok;
+        expect(client).to.eql({
+              _id: 'clientId',
+              _secret: 'clientSecret',
+              _accessToken: 'accessToken',
+              _refreshToken: 'refreshToken' 
+            })
+    });
+  });
+});
