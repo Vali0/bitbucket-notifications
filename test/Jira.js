973d4f338f784227139b58fa8efccaca3c4758ef
Valentin <valentin.radev@blue-leaf.co.uk>
2018-05-18 18:43:18 +0300

    Added missing get and set methods. Added unit tests

--- test/Jira.js
+++ test/Jira.js
@@ -1,11 +1,23 @@
-let expect = require('chai').expect;
+let chai = require('chai'),
+    chaiAsPromised = require('chai-as-promised'),
+    sinon = require('sinon'),
+    sinonStubPromise = require('sinon-stub-promise'),
+    proxyquire = require('proxyquire'),
+    expect = chai.expect;
+
+sinonStubPromise(sinon);
+chai.use(chaiAsPromised);
 
 describe('Jira', function() {
     let Jira,
-        domain;
+        domain,
+        username,
+        authorisationToken;
 
     beforeEach(function() {
         domain = 'myJira';
+        username = 'jane.doe';
+        authorisationToken = 'token666';
     });
 
     describe('constructor', function() {
@@ -20,9 +32,17 @@ describe('Jira', function() {
             let jira = new Jira();
 
             // assert
-            expect(jira).to.eql({
-                domain: undefined
-            });
+            expect(jira._domain).to.be.undefined;
+        });
+
+        it('should not add transition url if domain is missing', function() {
+            // arrange
+
+            // act
+            let jira = new Jira();
+
+            // assert
+            expect(jira._transitionUrl).to.be.undefined;
         });
 
         it('should create Jira instance by given domain name', function() {
@@ -33,8 +53,27 @@ describe('Jira', function() {
 
             // assert
             expect(jira).to.eql({
-                domain: domain,
-                browseUrl: 'https://myJira.atlassian.net/browse/{ticketId}'
+                _domain: domain,
+                _username: undefined,
+                _authorisationToken: undefined,
+                _browseUrl: 'https://myJira.atlassian.net/browse/{ticketId}',
+                _transitionUrl: 'https://myJira.atlassian.net/rest/api/2/issue/{issueIdOrKey}/transitions'
+            });
+        });
+
+        it('should create Jira instance by given domain name, username and authorisation token', function() {
+            // arrange
+
+            // act
+            let jira = new Jira(domain, username, authorisationToken);
+
+            // assert
+            expect(jira).to.eql({
+                _domain: domain,
+                _username: username,
+                _authorisationToken: authorisationToken,
+                _browseUrl: 'https://myJira.atlassian.net/browse/{ticketId}',
+                _transitionUrl: 'https://myJira.atlassian.net/rest/api/2/issue/{issueIdOrKey}/transitions'
             });
         });
     });
@@ -76,6 +115,101 @@ describe('Jira', function() {
 
             // assert
             expect(browseUrl).to.equal(expected);
+        });
+    });
+
+    describe('transitionIssue', function() {
+        let promise;
+
+        beforeEach(function() {
+            promise = sinon.stub().returnsPromise();
+
+            Jira = proxyquire('../lib/Jira', {
+                'request-promise': promise
+            });
+        });
+
+        it('should throw an error if username is missing', function() {
+            // arrange
+
+            // act
+            let jira = new Jira(domain);
+            let result = function() {
+                jira.transitionIssue();
+            };
+
+            // assert
+            expect(result).to.throw('Jira username is missing. Please check your configuration');
+        });
+
+        it('should throw an error if authorisation token is missing', function() {
+            // arrange
+
+            // act
+            let jira = new Jira(domain, username);
+            let result = function() {
+                jira.transitionIssue();
+            };
+
+            // assert
+            expect(result).to.throw('Jira authorisation token is missing. Please check your configuration');
+        });
+
+        it('should throw an error if issue id is not passed to the function', function() {
+            // arrange
+
+            // act
+            let jira = new Jira(domain, username, authorisationToken);
+            let result = function() {
+                jira.transitionIssue();
+            };
+
+            // assert
+            expect(result).to.throw('Issue id is missing');
+        });
+
+        it('should throw an error if transition id is not passed to the function', function() {
+            // arrange
+            let issueId = 'FOO-666';
+
+            // act
+            let jira = new Jira(domain, username, authorisationToken);
+            let result = function() {
+                jira.transitionIssue(issueId);
+            };
+
+            // assert
+            expect(result).to.throw('Transition id is missing');
+        });
+
+        it('should throw an error if request cannot be fulfilled', function() {
+            // arrange
+            let issueId = 'FOO-666',
+                transitionId = '323';
+
+            promise.rejects('bad request');
+
+            // act
+            let jira = new Jira(domain, username, authorisationToken);
+            let result = jira.transitionIssue(issueId, transitionId);
+
+            // assert
+            expect(result.rejectValue.toString()).to.equal('Error: Can not transition issue FOO-666 to 323. Stack trace: bad request');
+        });
+
+        it('should return empty response if successful', function() {
+            // arrange
+            let issueId = 'FOO-666',
+                transitionId = '323';
+
+            promise.resolves();
+
+            // act
+            let jira = new Jira(domain, username, authorisationToken);
+            let result = jira.transitionIssue(issueId, transitionId);
+
+            // assert
+            expect(result.resolveValue).to.be.undefined;
         });
     });
 });
