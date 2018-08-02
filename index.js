e77c2f2b4a2b9245f3e699ec3dc48be852a6661f
Valentin <valentin.radev@blue-leaf.co.uk>
2018-08-02 13:32:56 +0300

    Updated index.js Bitbucket.js Gmail.js and Jira.js to use new config file.

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 var bitbucketNotifications = (function() {
     'use strict';
 
-    const fs = require('fs');
     const path = require('path');
+    const configModule = require('./lib/config');
 
     const currentWorkingDirectory = process.cwd();
     const configurationPath = path.join(currentWorkingDirectory, 'config/default.json');
-
-    // Personally I don't find reason to put try-catch here as error from fs.readFileSync itself is descriptive
-    var configurationJSON = fs.readFileSync(configurationPath, 'utf8');
-
-    var config;
-    try {
-        config = JSON.parse(configurationJSON);
-    } catch (err) {
-        throw new Error('Cant not parse configuration. Stack trace: ' + err);
-    }
+    let config = configModule.read(configurationPath);
 
     const bitbucketConfig = config.bitbucket;
     const gmailConfig = config.gmail;
@@ -25,19 +16,9 @@ var bitbucketNotifications = (function() {
     const Gmail = require('./lib/Gmail');
     const Jira = require('./lib/Jira');
 
-    // No need to check for access token as it expires
-    if (!(bitbucketConfig.clientId && bitbucketConfig.clientSecret && bitbucketConfig.refreshToken)) {
-        throw new Error('Missing OAuth2 configuration for bitbucket in config.json');
-    }
-
-    // No need to check for access token as it expires
-    if (!(gmailConfig.user && gmailConfig.clientId && gmailConfig.clientSecret && gmailConfig.refreshToken)) {
-        throw new Error('Missing OAuth2 configuration for gmail in config.json');
-    }
-
-    var bitbucket = new Bitbucket(bitbucketConfig.clientId, bitbucketConfig.clientSecret, bitbucketConfig.accessToken, bitbucketConfig.refreshToken);
-    var gmail = new Gmail(gmailConfig.user, gmailConfig.clientId, gmailConfig.clientSecret, gmailConfig.accessToken, gmailConfig.refreshToken);
-    var jira = new Jira(jiraConfig.domain, jiraConfig.username, jiraConfig.authorisationToken);
+    let bitbucket = new Bitbucket(bitbucketConfig);
+    let gmail = new Gmail(gmailConfig);
+    let jira = new Jira(jiraConfig);
 
     return {
         bitbucket: bitbucket,
